8eee0554269fbdc4acaf8f1ea55a8ad012d65dca
khakayi-hub <khakamisty@gmail.com>
2026-03-19 10:37:57 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// Variable declarations
+let name = "Mitchelee Khakayi";
+let age = 21;
+const birthYear = 2004;
+
+// typeof operator
+console.log(typeof name);    // string
+console.log(typeof age);     // number
+console.log(typeof true);    // boolean
+
+// let vs const
+let score = 100;
+score = 150;  // Works!
+
+const PI = 3.14159;
+// PI = 3;  // Error! const cannot be reassigned
