5c29562c537bd6853ca97dc38b3d49383769a23c
khakayi-hub <khakamisty@gmail.com>
2026-03-19 11:13:41 +0000

    Add commit

--- main.js
+++ main.js
@@ -14,3 +14,17 @@ score = 150;  // Works!
 
 const PI = 3.14159;
 // PI = 3;  // Error! const cannot be reassigned
+
+// Declare variables
+let name = "Mitchelle Khakayi";
+let age = 21;
+let isStudent = true;
+let favoriteColors = ["blue", "black", "white"];
+let todayDate = new Date();
+
+// Output to console
+console.log("My name is: " + name);
+console.log("I am " + age + " years old");
+console.log("Am I a student? " + isStudent);
+console.log("My favorite colors are: " + favoriteColors.join(", "));
+console.log("Today's date is: " + todayDate);
